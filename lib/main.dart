982d2039ce2955ed777de63777d031fabfcea23e
Kojo Kwakye <kodeon6@gmail.com>
2019-05-06 00:36:21 +0100

    Connected both dice

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
           title: Text('Idicee'),
           backgroundColor: Colors.red,
@@ -25,6 +25,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  Future rowDice() async {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,7 +41,7 @@ class _DicePageState extends State<DicePage> {
               child: FlatButton(
                 onPressed: () {
                   setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rowDice();
                   });
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -46,7 +51,7 @@ class _DicePageState extends State<DicePage> {
               child: FlatButton(
                 onPressed: () {
                   setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
+                    rowDice();
                   });
                 },
                 child: Image.asset('images/dice$rightDiceNumber.png'),
